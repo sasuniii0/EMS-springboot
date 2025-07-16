d1e46d278c40b3d08adbbf370413e26a9a627681
sasuniii0 <sasuniwijerathne@gmail.com>
2025-07-16 12:16:44 +0530

    initialize Spring Boot application with basic structure and configuration

---
+++ src/test/java/lk/ijse/gdse/springboot_practice/SpringbootPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.gdse.springboot_practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
