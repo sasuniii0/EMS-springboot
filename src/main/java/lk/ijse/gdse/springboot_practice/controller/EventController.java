fc989aeab0b13c35631d802a8cf60084095b95ee
sasuniii0 <sasuniwijerathne@gmail.com>
2025-07-16 12:57:21 +0530

    add EventController, EventRepository, EventService, and EventServiceImpl for event management

---
+++ src/main/java/lk/ijse/gdse/springboot_practice/controller/EventController.java
@@ -0,0 +1,11 @@
+package lk.ijse.gdse.springboot_practice.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("api/v1/event")
+@RestController
+@RequiredArgsConstructor
+public class EventController {
+}
---
+++ src/main/java/lk/ijse/gdse/springboot_practice/service/impl/EventServiceImpl.java
@@ -0,0 +1,10 @@
+package lk.ijse.gdse.springboot_practice.service.impl;
+
+import lk.ijse.gdse.springboot_practice.service.EventService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class EventServiceImpl implements EventService {
+}
