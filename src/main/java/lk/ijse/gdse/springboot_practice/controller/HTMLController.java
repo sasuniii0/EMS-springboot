5de14e2852d9e768387ed67699f94761c37e1d94
sasuniii0 <sasuniwijerathne@gmail.com>
2025-07-22 00:26:08 +0530

    feat: change HTMLController to use RestController for improved API handling

--- src/main/java/lk/ijse/gdse/springboot_practice/controller/HTMLController.java
+++ src/main/java/lk/ijse/gdse/springboot_practice/controller/HTMLController.java
@@ -1,9 +1,9 @@
 package lk.ijse.gdse.springboot_practice.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HTMLController {
     @GetMapping("/")
     public String index() {
