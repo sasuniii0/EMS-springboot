b1dbf08ebc543a57957c310f7336df2252b0f32e
sasuniii0 <sasuniwijerathne@gmail.com>
2025-08-01 22:46:04 +0530

    set up endpoint controllers

---
+++ Auth/BN-Auth/src/main/java/lk/ijse/gdse/bnauth/controller/HelloController.java
@@ -0,0 +1,23 @@
+package lk.ijse.gdse.bnauth.controller;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+
+    @GetMapping("/admin")
+    @PreAuthorize("hasRole('ADMIN')")
+    public String helloAdmin() {
+        return "Hello, Admin!";
+    }
+
+    @GetMapping("/user")
+    @PreAuthorize("hasRole('USER')")
+    public String helloUser() {
+        return "Hello, User!";
+    }
+}
