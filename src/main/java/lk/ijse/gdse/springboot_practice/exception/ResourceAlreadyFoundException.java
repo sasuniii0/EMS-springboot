945e84a6e59b6f0c62d7e4ec37d1991d223630a3
sasuniii0 <sasuniwijerathne@gmail.com>
2025-08-01 15:20:49 +0530

    jwt with fn and bn

--- src/main/java/lk/ijse/gdse/springboot_practice/exception/ResourceAlreadyFoundException.java
+++
@@ -1,7 +0,0 @@
-package lk.ijse.gdse.springboot_practice.exception;
-
-public class ResourceAlreadyFoundException extends RuntimeException {
-    public ResourceAlreadyFoundException(String message) {
-        super(message);
-    }
-}
--- src/main/java/lk/ijse/gdse/springboot_practice/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package lk.ijse.gdse.springboot_practice.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
