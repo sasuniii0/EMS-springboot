0c7c7b9962bbbe3d937657f7163d35927500bd8f
sasuniii0 <sasuniwijerathne@gmail.com>
2025-08-01 22:21:23 +0530

    after creating util package and add UserRepository class

---
+++ Auth/BN-Auth/src/main/java/lk/ijse/gdse/bnauth/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package lk.ijse.gdse.bnauth.repository;
+
+import lk.ijse.gdse.bnauth.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+}
