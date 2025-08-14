36cc67c1063152acb338426e2d9229a004b3a91e
sasuniii0 <sasuniwijerathne@gmail.com>
2025-08-14 08:47:40 +0530

    Initialize Spring Boot application with Maven wrapper and basic configuration for Google authentication

---
+++ demoAuthGoogle/src/main/java/com/example/demoAuthGoogle/DemoAuthGoogleApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoAuthGoogle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoAuthGoogleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoAuthGoogleApplication.class, args);
+	}
+
+}
---
+++ demoAuthGoogle/src/test/java/com/example/demoAuthGoogle/DemoAuthGoogleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoAuthGoogle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoAuthGoogleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
